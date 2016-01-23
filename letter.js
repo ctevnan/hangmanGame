e67853ed821777c4a2735796e9437e8b14db8204
ctevnan <carolyntevnan@yahoo.com>
2016-01-23 13:09:01 -0500

    step one of hangman game

---
+++ letter.js
@@ -0,0 +1,14 @@
+var Letter
+function Letter(let){
+this.carac = let;
+this.appear=function(){
+if (charac.appear === true){
+console.log ("Whats up");
+this.appear=false;
+}else{
+console.log("Goodbye");
+this.appear = true;
+}
+};
+
+module.exports = letters
