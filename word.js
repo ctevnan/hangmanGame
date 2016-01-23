d5fc90e0c709cea2da549dc0c60102542aaa6c1f
ctevnan <carolyntevnan@yahoo.com>
2016-01-23 15:31:41 -0500

    created word.js file and started adding commenting out my steps

---
+++ word.js
@@ -0,0 +1,14 @@
+//import the letter.js file by using require, set it to a variable named letter
+
+//create a constructor function named Word
+
+
+//the constructor function named word should take an argument wrd
+//create a property called word which will store the argument wrd
+//create a property called lets which is set to an empty array ( "," )
+//create a property called found which is set to false (this inv. boolean)
+
+
+//create a function inside of Word constructor called getLets
+
+//
